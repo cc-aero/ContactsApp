654a042a3fae4867bbeaa5dbf0632ea89ebbffc8
Cameron C <cc.innovations.uk@gmail.com>
2021-03-07 20:45:30 +0000

    Cool CLI

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                            "\n--------------------------=[Welcome to Contacts CLI!]=--------------------------\n");
+        while(true){
+            System.out.println(
+                            "\n                               Choose an Option:"+
+                            "\n\n                                1. Add Contact"+
+                            "\n                                2. View Contacts"+
+                            "\n                                3. Exit");
+            System.out.println("\nccli>");
+            break;
+        }
+    }
+}
