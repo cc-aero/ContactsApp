bebc8e69bbad88c323dce9add518c68d1b46e5af
Cameron C <cc.innovations.uk@gmail.com>
2021-03-07 21:16:16 +0000

    Made CLI a bit cooler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private List<Contact> contactList;
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static void addContactInterface(){
+        String contactFirstName;
+        System.out.println( "\n--------------------------------=[Add  Contact]=--------------------------------\n");
+        System.out.print( "                              Enter Contact First Name:\n\n");
+        System.out.print("addcontact>");
+        contactFirstName = scanner.next();
+
+    }
+
     public static void main(String[] args) {
+        String command;
         System.out.println(
                             "\n--------------------------=[Welcome to Contacts CLI!]=--------------------------\n");
         while(true){
@@ -8,8 +24,12 @@ public class Main {
                             "\n\n                                1. Add Contact"+
                             "\n                                2. View Contacts"+
                             "\n                                3. Exit");
-            System.out.println("\nccli>");
-            break;
+            System.out.print("\nccli>");
+            command = scanner.next();
+            if(command.equals("3")){break;}
+            if(command.equals("2")){break;}
+            if(command.equals("1")){addContactInterface();}
+
         }
     }
 }
